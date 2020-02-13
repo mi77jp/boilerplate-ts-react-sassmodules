0f2f7d713cb902644a0743083d863faea8fb27fc
Hiroyuki Miyashita <hiroyuki.miyashita@nex.nikkei.com>
2020-02-13 16:47:27 +0900

    Routing base

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,23 @@ import "core-js";
 import "regenerator-runtime/runtime";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
 
 import Stage from "./components/stage/";
 const styles = require('./styles/global.scss');
 
 ReactDOM.render(
   <div className={ styles.global } >
-    <Stage name="Max" />
+    <HashRouter>
+      <Switch>
+        <Route path='/' exact>
+          <Stage name="Home" />
+        </Route>
+        <Route path='/result'>
+          <Stage name="Result" />
+        </Route>
+      </Switch>
+    </HashRouter>
   </div>,
   document.getElementById("app"),
 );
