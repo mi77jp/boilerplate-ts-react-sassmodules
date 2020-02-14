b15943f652bcb50a27e0e7e9dd4468db9bcf3e64
Hiroyuki Miyashita <hiroyuki.miyashita@nex.nikkei.com>
2020-02-14 16:00:30 +0900

    Fix

--- src/util/csv2json.ts
+++ src/util/csv2json.ts
@@ -1,9 +1,9 @@
-export const csv2json = (str: String) => {
-  const delimiter = ",";
-  return str.split("\n").reduce((table, row) => {
+export const csv2json = (str: any) => {
+  const delimiter: any = ",";
+  return str.split("\n").reduce((table: any, row: any) => {
     if (!table) return;
     table.push(
-      row.split(delimiter).map(d => {
+      row.split(delimiter).map( (d: any) => {
         return d.trim();
       })
     );
