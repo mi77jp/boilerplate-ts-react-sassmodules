41962a61384384488d4512edb2fe9b8af35d7d96
Hiroyuki Miyashita <miya1977@gmail.com>
2020-02-15 02:33:19 +0900

    ReactN

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,52 @@
 import "core-js";
 import "regenerator-runtime/runtime";
-import * as React from "react";
+import React, { useState, useEffect, useGlobal } from 'reactn';
 import * as ReactDOM from "react-dom";
-import { Provider as ReduxProvider  } from 'react-redux';
-import store from './modules/store';
-import App from './app';
+import axios from 'axios';
+import { csv2json } from './util/csv2json';
+//import App from './app';
+
+const QAS_URL = "./data.csv";
+const App: any = () => {
+
+  const [qas, setQas] = useState({});
+  useEffect( () => {
+    axios.get(QAS_URL).then((res: any) => {
+      setQas( csv2json(res.data) );
+      setTitle('done');
+    });
+  }, [setQas]);
+
+  const [title, setTitle] = useGlobal<any>('page');
+  useEffect(() => {
+    if (title) {
+      document.title = `App "${ title }"`;
+    } else {
+      document.title = 'App';
+    }
+  }, [title]);
+
+  const renderQa = Object.values(qas).map( (qa: any) => {
+    return (
+      <li key={qa}>
+        { console.log(qa) }
+        { qa[2] }
+      </li>
+    );
+  });
+
+  return (
+    <ul>
+      { renderQa }
+    </ul>
+  );
+};
+
+//////
 
 ReactDOM.render(
-  <ReduxProvider store={store}>
+  <>
     <App />
-  </ReduxProvider>,
+  </>,
   document.getElementById("app") as HTMLElement
 );
