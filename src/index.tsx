bdb8d8db444a09da1c8fa9e94108b3e3710d64a6
Hiroyuki Miyashita <hiroyuki.miyashita@nex.nikkei.com>
2020-02-17 13:58:13 +0900

    Change data

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,20 @@ import axios from 'axios';
 import { csv2json } from './util/csv2json';
 //import App from './app';
 
-const QAS_URL = "./data.csv";
+const Q_URL = "./data/q.csv";
+//const A_URL = "./data/a.csv";
 const App: any = () => {
 
-  const [qas, setQas] = useState({});
+  // load data
+  const [qa, setQa] = useGlobal<any>();
   useEffect( () => {
-    axios.get(QAS_URL).then((res: any) => {
-      setQas( csv2json(res.data) );
+    axios.get(Q_URL).then((res: any) => {
+      setQa( csv2json(res.data) );
       setTitle('done');
     });
-  }, [setQas]);
+  }, []);
 
-  const [title, setTitle] = useGlobal<any>('page');
+  const [title, setTitle] = useState<any>('page');
   useEffect(() => {
     if (title) {
       document.title = `App "${ title }"`;
@@ -26,7 +28,7 @@ const App: any = () => {
     }
   }, [title]);
 
-  const renderQa = Object.values(qas).map( (qa: any) => {
+  const renderQa = Object.values(qa).map( (qa: any) => {
     return (
       <li key={qa}>
         { console.log(qa) }
