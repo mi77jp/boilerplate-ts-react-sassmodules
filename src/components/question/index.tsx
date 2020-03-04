9bfca82cf8f3cbf3eb9b11a3e04d62691c8107b9
Hiroyuki Miyashita <hiroyuki.miyashita@nex.nikkei.com>
2020-03-04 09:51:47 +0900

    Add form lib

--- src/components/question/index.tsx
+++ src/components/question/index.tsx
@@ -1,4 +1,5 @@
 import React, { useGlobal } from 'reactn';
+import { useForm } from 'react-hook-form'
 import classNames from 'classnames';
 const styles = require('./index.scss');
 
@@ -15,11 +16,46 @@ export const Question: React.FC<QuestionProps> = props => {
     styles.active
   );
 
+  const { register, handleSubmit, watch, errors } = useForm()
+  const onSubmit = data => { console.log('onSubmit: ' + data) }
+
+  console.log(watch('example'));
+
   return (
     <div className={ wrapperClassNames }>
-      { console.log(question) }
+      {
+        //console.log(question)
+      }
       <h4>{ props.show }番目の質問</h4>
       <p>{ question }</p>
+
+      <hr />
+
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <input
+          name="example"
+          defaultValue=""
+          ref={register} />
+        <hr />
+
+        <input
+          name="exampleRequired"
+          ref={register({ required: true })} />
+        <hr />
+
+        <input
+          name="age"
+          type="number"
+          ref={register({ min: 18, max: 99 })} />
+        <hr />
+
+        <p >{errors.exampleRequired && <span>This field is required</span>}</p>
+        <p >{errors.age             && <span>age must be number between 18 to 99.</span>}</p>
+
+        <input type="submit" value="submit" />
+       </form>
     </div>
   );
 };
